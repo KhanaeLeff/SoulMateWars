09fcd158d3eb0b4edf9bdcc5d1450c5e76343518
KhanaeLeff <scenesdecrime@gmail.com>
2018-05-17 16:21:39 +0200

    title + bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg'
+import logo from './logo.svg';
+import './bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">SoulMate</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">SoulMateWars</h1>
+
         </header>
         <p className="App-intro">  </p>
         <body>
