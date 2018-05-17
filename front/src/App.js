a9d1b17381dfc43e1e1eb5c9495c747d612f4976
Alexandre Morazin <alexandre.morazin@live.fr>
2018-05-17 17:01:54 +0200

    ajout formulaire + story

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Profil from './Profil'
+import './bootstrap.min.css'
 import './App.css';
 
 class App extends Component {
@@ -8,11 +10,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">SoulMateWars</h1>
+          <h1 className="App-title">by SoulMateWars</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Profil />
       </div>
     );
   }
---
+++ front/src/Profil.js
@@ -0,0 +1,63 @@
+import React from 'react';"formulaire"
+
+class Profil extends React.Component {
+  constructor(props) {
+    super(props)
+    this.updateProfilPrenom = this.updateProfilPrenom.bind(this)
+    this.state = {prenom: "coeur a prendre", nom:"", age:"", sexe:"", interet:""}
+    this.updateProfilNom = this.updateProfilNom.bind(this)
+    this.updateProfilAge = this.updateProfilAge.bind(this)
+    this.updateProfilSexe = this.updateProfilSexe.bind(this)
+    this.updateProfilInteret = this.updateProfilInteret.bind(this)
+  }
+
+  updateProfilPrenom(event) {
+    this.setState({prenom: event.target.value})
+  }
+  updateProfilNom(event) {
+    this.setState({nom: event.target.value})
+  }
+  updateProfilAge(event) {
+    this.setState({age: event.target.value})
+  }
+  updateProfilSexe(event) {
+    this.setState({sexe: event.target.value})
+  }
+  updateProfilInteret(event) {
+    this.setState({interet: event.target.value})
+  }
+
+  render() {
+    const inputStyle = {
+      color: "#FF33F6",
+    }
+    return(
+      <div className="row">
+        <div className="formulaire col-md-6">
+          <input style={inputStyle} type="prenom" className="form-control" value={this.state.value} onChange={this.updateProfilPrenom} placeholder="Votre prénom"/>
+          <br />
+          <input style={inputStyle} type="nom" className="form-control" value={this.state.value} onChange={this.updateProfilNom} placeholder="Votre nom"/>
+          <br />
+          <input style={inputStyle} type="age" className="form-control" value={this.state.value} onChange={this.updateProfilAge} placeholder="Votre âge" />
+          <br />
+          <input style={inputStyle} type="sexe" className="form-control" value={this.state.value} onChange={this.updateProfilSexe} placeholder="Votre sexe" />
+          <br />
+          <label for="">Je suis intéressé(e) par...</label>
+            <select className="form-control" id="exampleFormControlSelect1" style={inputStyle} value={this.state.value} onChange={this.updateProfilInteret}>
+              <option>une femme</option>
+              <option>un homme</option>
+              <option>un droïde</option>
+            </select>
+        </div>
+        <div className="story col-md-6">
+          <h1>Bonjour {this.state.prenom}</h1>
+          <p>Puissante la famille {this.state.nom} est, dans la force !</p>
+          <p>Mais seul, aujourd'hui tu te trouves...</p>
+          <p>Tu nous fait part de ton intérêt pour {this.state.interet}</p>
+        </div>
+      </div>
+
+    )
+  }
+}
+export default Profil;
