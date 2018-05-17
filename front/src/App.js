551adc135846faec4a7003945904829832060e65
KhanaeLeff <scenesdecrime@gmail.com>
2018-05-17 14:34:25 +0200

    background image

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <body>
+          
+        </body>
       </div>
+
     );
   }
 }
